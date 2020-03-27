d92761c5eac049fb1019811ce8fa61c2ea340a1a
palfore <nawar@palfore.com>
2020-03-27 16:03:43 -0400

    Added hits.py tests, added attacks_until_kill method

--- Code/osrsmath/model/hits.py
+++ Code/osrsmath/model/hits.py
@@ -1,26 +1,36 @@
 """ This module adds to successful_hits.py by accounting for accuracy, a.
-	The method for successful hit calculation can be chosen, and the accuracy
+	The model for successful hit calculation can be chosen, and the accuracy
 	modifies the answer appropriately. """
 
 import numpy as np
 
-def health_after_attacks(n, h_0, m, a, method):
+def health_after_attacks(n, h_0, M, a, model):
 	""" Calculates the health of an opponent after a certain number of attacks.
 		@param n The number of turns to attack for.
 		@param h_0 The initial health of the opponent.
-		@param m The max hit of the attacker.
+		@param M The max hit of the attacker.
 		@param a The accuracy of the attacker.
-		@param method The calculation method. @see successful_hits.py. """
+		@param model The successful hits class. @see successful_hits.py. """
 	assert 0 <= a <= 1, a
-	return method.h(n*a, h_0, m)
+	return model.h(n*a, h_0, M)
 
-def attacks_until_health(h, h_0, m, a, method):
+def attacks_until_health(h, h_0, M, a, model):
 	""" Calculates the number of turns to get an opponent to a given health.
 		@param h The final health of the opponent.
 		@param h_0 The initial health of the opponent.
-		@param m The max hit of the attacker.
+		@param M The max hit of the attacker.
 		@param a The accuracy of the attacker.
-		@param method The calculation method. @see successful_hits.py. """
+		@param model The successful hits class. @see successful_hits.py. """
 	tolerance = 1e-12
 	assert tolerance <= a <= 1, a
-	return method.hinv(h, h_0, m) / a
+	return model.hinv(h, h_0, M) / a
+
+def attacks_until_kill(h_0, M, a, model):
+	""" Calculates the number of turns to get an opponent to a given health.
+		@param h_0 The initial health of the opponent.
+		@param M The max hit of the attacker.
+		@param a The accuracy of the attacker.
+		@param model The successful hits class. @see successful_hits.py. """
+	tolerance = 1e-12
+	assert tolerance <= a <= 1, a
+	return model.turns_to_kill(h_0, M) / a
